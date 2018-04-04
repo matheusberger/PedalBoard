04a308dc74540ae7bad662c1160593bc26ed6f10
mcb3 <matheuscberger@gmail.com>
2018-04-04 00:42:22 -0300

    auth layout missing typography

---
+++ PedalPrototype/Screens/Custom UI/UILabelExtension.swift
@@ -0,0 +1,42 @@
+//
+//  UILabelExtension.swift
+//  PedalPrototype
+//
+//  Created by Matheus Coelho Berger on 04/04/18.
+//  Copyright © 2018 mcb3. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    
+    @IBInspectable
+    var letterSpace: CGFloat {
+        set {
+            let attributedString: NSMutableAttributedString!
+            if let currentAttrString = attributedText {
+                attributedString = NSMutableAttributedString(attributedString: currentAttrString)
+            }
+            else {
+                attributedString = NSMutableAttributedString(string: text ?? "")
+                text = nil
+            }
+            
+            attributedString.addAttribute(kCTKernAttributeName as NSAttributedStringKey,
+                                          value: newValue,
+                                          range: NSRange(location: 0, length: attributedString.length))
+            
+            attributedText = attributedString
+        }
+        
+        get {
+            if let currentLetterSpace = attributedText?.attribute(kCTKernAttributeName as NSAttributedStringKey, at: 0, effectiveRange: .none) as? CGFloat {
+                return currentLetterSpace
+            }
+            else {
+                return 0
+            }
+        }
+    }
+}
