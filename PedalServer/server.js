fea063e7c3042f0b67d64dc65eac364bc9c149ea
Daniel Barbosa Maranhão <dbm2@cin.ufpe.br>
2018-04-23 00:49:20 -0300

    Trying to fix SSL

--- PedalServer/server.js
+++ PedalServer/server.js
@@ -2,6 +2,8 @@ var app = require('./app');
 var fs = require('fs');
 var https = require('https');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 var port = process.env.PORT || 3000;
 
 var key = fs.readFileSync('encryption/key.pem');
